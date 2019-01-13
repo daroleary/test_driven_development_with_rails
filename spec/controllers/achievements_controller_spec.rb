4064bd8a1b937e230dbe291e9a2fcd345f4f9134
darren.oleary <Darren.O@trufla.com>
2019-01-13 13:19:01 +0800

    Add authentication to app using devise gem

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -2,29 +2,68 @@ require 'rails_helper'
 
 describe AchievementsController do
 
-  describe "GET index" do
-    it "renders :index template" do
-      get :index
-      expect(response).to render_template(:index)
-    end
-    it "assigns only public achievements to template" do
-      public_achievement = FactoryGirl.create(:public_achievement)
-      FactoryGirl.create(:private_achievement)
+  describe "guest user" do
+    describe "GET index" do
+      it "renders :index template" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+      it "assigns only public achievements to template" do
+        public_achievement = FactoryGirl.create(:public_achievement)
+        FactoryGirl.create(:private_achievement)
 
-      get :index
-      expect(assigns(:achievements)).to match_array([public_achievement])
-    end
-  end
-
-  describe "GET new" do
-    it 'renders :new template' do
-      get :new
-      expect(response).to render_template(:new)
+        get :index
+        expect(assigns(:achievements)).to match_array([public_achievement])
+      end
     end
 
-    it 'assigns new Achievement to @achievement' do
-      get :new
-      expect(assigns(:achievement)).to be_a_new(Achievement)
+    describe "GET show" do
+      let(:achievement) { FactoryGirl.create(:public_achievement) }
+
+      it "renders :show template" do
+        get :show, params: { id: achievement }
+        expect(response).to render_template(:show)
+      end
+
+      it "assigns requested achievement to @achievement" do
+        get :show, params: { id: achievement }
+        expect(assigns(:achievement)).to eq(achievement)
+      end
+    end
+
+    describe "GET new" do
+      it "redirects to login page" do
+        get :new
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe "POST create" do
+      it "redirects to login page" do
+        post :create, params: { achievement: FactoryGirl.attributes_for(:public_achievement) }
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe "GET edit" do
+      it "redirects to login page" do
+        get :edit, params: { id: FactoryGirl.create(:public_achievement) }
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe "PUT update" do
+      it "redirects to login page" do
+        put :update, params: { id: FactoryGirl.create(:public_achievement), achievement: FactoryGirl.attributes_for(:public_achievement) }
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "redirects to login page" do
+        delete :destroy, params: { id: FactoryGirl.create(:public_achievement) }
+        expect(response).to redirect_to(new_user_session_url)
+      end
     end
   end
 
@@ -39,7 +78,18 @@ describe AchievementsController do
       get :edit, params: { id: achievement }
       expect(assigns(:achievement)).to eq(achievement)
     end
+  end
 
+  describe "GET new" do
+    it 'renders :new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it 'assigns new Achievement to @achievement' do
+      get :new
+      expect(assigns(:achievement)).to be_a_new(Achievement)
+    end
   end
 
   describe "PUT update" do
@@ -85,20 +135,6 @@ describe AchievementsController do
     it "deletes achievement from the database" do
       delete :destroy, params: { id: achievement }
       expect(Achievement.exists?(achievement.id)).to be_falsey
-    end
-  end
-
-  describe "GET show" do
-    let(:achievement) { FactoryGirl.create(:public_achievement) }
-
-    it "renders :show template" do
-      get :show, params: { id: achievement }
-      expect(response).to render_template(:show)
-    end
-
-    it "assigns requested achievement to @achievement" do
-      get :show, params: { id: achievement }
-      expect(assigns(:achievement)).to eq(achievement)
     end
   end
 
